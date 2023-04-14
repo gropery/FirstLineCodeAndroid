97bbab3e0f80f53e75cb0d3abf0944ef6141d4fc
gropery <gropery@163.com>
2023-04-14 14:31:41 +0800

    完成遍历省市县三级列表的功能

--- P14_CoolWeather/app/src/main/java/com/coolweather/android/util/Utility.java
+++ P14_CoolWeather/app/src/main/java/com/coolweather/android/util/Utility.java
@@ -21,6 +21,7 @@ public class Utility {
                 JSONArray allProvinces = new JSONArray(response);
                 for (int i = 0;i < allProvinces.length();i++) {
                     JSONObject provinceObject = allProvinces.getJSONObject(i);
+                    // 写数据库表
                     Province province = new Province();
                     province.setProvinceName(provinceObject.getString("name"));
                     province.setProvinceCode(provinceObject.getInt("id"));
@@ -35,6 +36,8 @@ public class Utility {
     }
     /**
      * 解析和处理服务器返回的市级数据
+     * 以JSONObject方式解析json数据, 并将解析后的数据写入数据库的City表
+     * 注意: 第二个参数传入了City所属的provinceId
      */
     public static boolean handleCityResponse(String response,int provinceId) {
         if (! TextUtils.isEmpty(response)) {
@@ -42,6 +45,7 @@ public class Utility {
                 JSONArray allCities = new JSONArray(response);
                 for (int i = 0; i < allCities.length(); i++) {
                     JSONObject cityObject = allCities.getJSONObject(i);
+                    // 写数据库表
                     City city = new City();
                     city.setCityName(cityObject.getString("name"));
                     city.setCityCode(cityObject.getInt("id"));
@@ -57,6 +61,8 @@ public class Utility {
     }
     /**
      * 解析和处理服务器返回的县级数据
+     * 以JSONObject方式解析json数据, 并将解析后的数据写入数据库的County表
+     * 注意: 第二个参数传入了County所属的cityId
      */
     public static boolean handleCountyResponse(String response,int cityId) {
         if (! TextUtils.isEmpty(response)) {
@@ -64,6 +70,7 @@ public class Utility {
                 JSONArray allCounties = new JSONArray(response);
                 for (int i = 0; i < allCounties.length(); i++) {
                     JSONObject countyObject = allCounties.getJSONObject(i);
+                    // 写数据库表
                     County county = new County();
                     county.setCountyName(countyObject.getString("name"));
                     county.setWeatherId(countyObject.getString("weather_id"));
